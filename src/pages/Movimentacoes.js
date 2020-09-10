9d3e932e9ca52d3588f15d2db71a94eed647b11c
Breno Cristovão Rocha <67420626+brenocristovaorocha@users.noreply.github.com>
2020-09-10 18:13:32 -0300

    Add files via upload

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {useState} from 'react';
 import Rest from '../utils/rest.js';
 const baseUrl = 'https://mymoney-brenorocha.firebaseio.com/';
-const { useGet, usePost, useDelete } = Rest(baseUrl)
+const { useGet, usePost, useDelete, usePatch } = Rest(baseUrl)
 
 const Movimentacoes = ({match}) => {
 
     const data = useGet(`movimentacoes/${match.params.data}`);
+    const dataMeses = useGet(`meses/${match.params.data}`);
+
+    const [dataPatch,patch] = usePatch();
+
     const [postData,salvar] = usePost(`movimentacoes/${match.params.data}`);
     const [removeData, remover] = useDelete();
     const [descricao,setDescricao] = useState('');
@@ -29,6 +33,7 @@ const Movimentacoes = ({match}) => {
         setDescricao('');
         setValor(0);
         data.refetch();
+        dataMeses.refetch();
 
       }
       
@@ -37,11 +42,26 @@ const Movimentacoes = ({match}) => {
     const removerMovimentacao = async(id) => {
       await remover(`movimentacoes/${match.params.data}/${id}`)
       data.refetch();
+      dataMeses.refetch();
+    }
+
+    const alterarPrevisaoEntrada = (evt) => {
+      patch(`meses/${match.params.data}`,{ previsao_entrada: evt.target.value })
+    }
+
+    const alterarPrevisaoSaida = (evt) => {
+      patch(`meses/${match.params.data}`,{ previsao_saida: evt.target.value })
     }
 
     return (
       <div className="container">
         <h1>Movimentações</h1>
+        {
+          !dataMeses.loading && <div>
+            Previsão de Entrada : {dataMeses.data.previsao_entrada} <input type='text' onBlur={alterarPrevisaoEntrada} /> / Previsão de Saída: {dataMeses.data.previsao_saida} <input type='text' onBlur={alterarPrevisaoSaida} /> <br/>
+            Entradas : {dataMeses.data.entradas} / Saidas : {dataMeses.data.saidas} 
+            </div>
+        }
         <table className="table">
           <thead>
             <tr>
